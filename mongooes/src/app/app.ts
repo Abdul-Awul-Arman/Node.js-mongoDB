45156f8716b2764898e6a976bf1bda9cae53f5c7
Abdul-Awul-Arman <abdulawularman52114@gmail.com>
2025-06-17 12:58:02 +0600

    learn mongoose schema

--- mongooes/src/app/app.ts
+++ mongooes/src/app/app.ts
@@ -1,22 +1,43 @@
 import express, { Application, Request, Response } from 'express'
 import { model, Schema } from 'mongoose';
 const app:Application=express();
+
+ app.use(express.json())
+
  const NoteSchema=new Schema({
-    title:String,
-    content:String
+    title:{type:String,require,trim:true},
+    content:{type:String,default:"No content available"},
+    category:{
+        type:String,
+        enum:["personal","study","other"],
+        default:"personal",
+    },
+    pinned:{
+        type:Boolean,
+        default:false
+
+    },
+
+    tags:{
+        label:{
+          type:String, require:true
+        },
+        color:{
+            type:String,
+            default:"gray", 
+            require:true
+        }
+    }
  });
  
   const Note=model("Note",NoteSchema);
 
 
-app.post("/create-note",async(req:Request,res:Response)=>{
-    const myNote=new Note({
-        title:"Learning mongoose",
-        content:"I am learning mongoose"
-    });
-
-  const result= await myNote.save()
-
+app.post("/note/create-note",async(req:Request,res:Response)=>{
+     
+   const body=req.body
+  
+   const myNote=await Note.create(body);
     res.status(201).send(
         {
             success:true,
